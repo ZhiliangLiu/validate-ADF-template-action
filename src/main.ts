3a33c2ebe0565e0ced88b8648555b394606c4279
Zoran Liu <zoranliu@microsoft.com>
2021-09-30 12:40:04 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ const main = () => {
     const PROJECT_ROOT = Deno.args[0]
     const TEMPLATES_PATH = `${PROJECT_ROOT}/${TEMPLATE_DIRECTORY_NAME}`
     validateDirectory(TEMPLATES_PATH)
+    console.log(TEMPLATES_PATH)
 }
 
 main()
