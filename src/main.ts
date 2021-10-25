fc5d1c78034c8fca1536a7280f2578fe5cf3ce56
Zoran Liu <zoranliu@microsoft.com>
2021-10-25 17:34:23 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import validate from './validate.ts';
 const TEMPLATE_DIRECTORY_NAME = 'templates';
 
 const main = () => {
-	console.log(Array.from(Deno.args))
+	console.log(Array.from(Deno.args), 12312)
 	// const PROJECT_ROOT = Deno.args[0];
 	// const TEMPLATES_PATH = `${PROJECT_ROOT}/${TEMPLATE_DIRECTORY_NAME}`;
 	// const result = validate(TEMPLATES_PATH);
