7fbe30136d4a906dfa2a81a37efedc55b23607e7
Zoran Liu <zoranliu@microsoft.com>
2021-11-03 11:25:22 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,12 @@ import validate from './validate.ts';
 const TEMPLATE_DIRECTORY_NAME = 'templates';
 
 const main = () => {
-	// const PROJECT_ROOT = Deno.args[0];
-	// const TEMPLATES_PATH = `${PROJECT_ROOT}/${TEMPLATE_DIRECTORY_NAME}`;
-	// const result = validate(TEMPLATES_PATH);
-	// Deno.writeAllSync(
-	// 	Deno.stdout,
-	// 	new TextEncoder().encode(JSON.stringify(result)),
-	// );
+	const PROJECT_ROOT = Deno.args[0];
+	const TEMPLATES_PATH = `${PROJECT_ROOT}/${TEMPLATE_DIRECTORY_NAME}`;
+	const result = validate(TEMPLATES_PATH);
 	Deno.writeAllSync(
 		Deno.stdout,
-		new TextEncoder().encode(JSON.stringify({ status: 1, detail: Array.from(Deno.args).toString() })),
+		new TextEncoder().encode(JSON.stringify(result)),
 	);
 };
 
